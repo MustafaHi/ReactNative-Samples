2f3c4b722656bedd778383a154cd0f3efe66c531
MustafaHi <matterik971@gmail.com>
2022-06-18 20:46:52 +0300

    +

--- card.tsx
+++ card.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { useState, useCallback } from "react";
 import { LinearGradient, LinearGradientProps } from "expo-linear-gradient";
 import { Text, StyleSheet, Pressable } from "react-native";
-
+import { Throttle } from "./utils";
 
 export default function Card({ background, onPress, title, text  }: { title: string, text: string, onPress: Function, background: LinearGradientProps }) {
+  // const onPressed = useCallback(
+  //   Throttle(onPress, 500), []
+  // );
   return (
     <LinearGradient {...background} style={s.container}>
-      <Pressable style={{height: '100%'}} onPress={() => onPress()}>
+      {/* <Pressable style={{height: '100%'}} onPress={() => onPressed()}> */}
+      <Pressable style={{height: '100%'}} onPress={Throttle(onPress, 500)}>
         <Text style={s.title}>Card: {title}</Text>
         <Text style={s.text}>{text}</Text>
       </Pressable>
---
+++ utils.ts
@@ -0,0 +1,13 @@
+
+export function Throttle(func: Function, delay: number)
+{
+  let prev = 0;
+  return (...args: any) => {
+    let now = new Date().getTime();
+    if (now - prev > delay) {
+      prev = now;
+      return func(...args);
+    }
+  }
+}
+

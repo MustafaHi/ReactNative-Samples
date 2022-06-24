b652365d2aff4096a15715de0abc8e451723fcd1
MustafaHi <matterik971@gmail.com>
2022-06-24 08:07:26 +0300

    +

---
+++ samples/data/Main.tsx
@@ -0,0 +1,13 @@
+import { useReducer } from "react";
+import { View, Text } from "react-native";
+
+export default function DataSample() {
+    
+
+    return(
+        <View>
+            <Text>Data Persistance</Text>
+        </View>
+    )
+}
+

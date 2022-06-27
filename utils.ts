b552384e7555d8321a5a6e6afa8ec87c20b23684
MustafaHi <matterik971@gmail.com>
2022-06-27 16:14:46 +0300

    +

--- utils.ts
+++ utils.ts
@@ -11,3 +11,12 @@ export function Throttle(func: Function, delay: number)
   }
 }
 
+export function Debounce(func: Function, delay: number)
+{
+  let timer: NodeJS.Timeout;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, delay);
+  };
+}
+

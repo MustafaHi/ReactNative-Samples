551a828d20428b3f8218e137548062d53beb6aee
MustafaHi <matterik971@gmail.com>
2022-06-27 16:38:48 +0300

    +

--- samples/data/Main.tsx
+++ samples/data/Main.tsx
@@ -1,36 +1,52 @@
-import { useEffect, useReducer, useState } from "react";
-import { View, Text, TextInput, } from "react-native";
-import { Throttle } from "../../utils";
-import * as Store from './storage'
+import { useEffect, useReducer, useState, useMemo } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { Debounce } from "../../utils";
+import * as Store from './storage';
+
+function reducer(state: any, action: {name: string, value: any}) {
+  switch (action.name) {
+    case 'all':
+      return {...state, ...action.value};
+    case 'name':
+      return {...state, name: action.value};
+  }
+}
 
 export default function DataSample() {
-  const [data, setData] = useState({name: 'sample'});
+  // const [data, setD] = useState({name: 'sample'});
+  const [data, updateData] = useReducer(reducer, {name: 'sample'});
 
   console.log('data', data);
   useEffect(() => { (async() => {
-    const newData = await Store.get();
-    if   (newData)  setData(newData);
-    console.log('new Data', data);
+    try {
+      const newData = await Store.get();
+      if   (newData)
+        updateData({name: 'all', value: newData});
+      console.log('new Data', newData);
+      console.log('updated Data', data);
+    } catch(e) {
+      console.log('error Data', e);
+    }
   })()}, []);
-
-  function onChange(name: keyof typeof data, value: any) {
-    // console.log(name, value);
-    data[name] = value;
-    setData(data);
-    console.log("setData:", data);
-    Throttle(Store.set, 1000)(data);
+  
+  const save = useMemo(() => Debounce((data: object) => Store.set(data), 1000), []);
+  // function onChange(name: keyof typeof data, value: any) {
+  function onChange(name: string, value: any) {
+    updateData({name: name, value: value});
+    save(data);
   }
 
   return (
     <View>
       <Text>Data Persistance</Text>
       <Input name="name" value={data.name} onChange={onChange}/>
+      <Button title="Log Data" onPress={() => console.log(data)}/>
     </View>
   )
 }
 
 
 function Input({name, value, onChange}: {name: string, value: string, onChange: Function}) {
-  return <TextInput value={value} onChange={(value) => onChange(name, value.nativeEvent.text)}/>;
+  return <TextInput value={value} onChangeText={(text) => onChange(name, text)}/>;
 }
 
